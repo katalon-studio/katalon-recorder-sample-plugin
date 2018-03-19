208862977c72d32cf0d301586096069db2564d3f
Hai Minh Nguyen <dev.alex.88@gmail.com>
2018-03-19 10:19:00 +0700

    update

--- background.js
+++ background.js
@@ -3,16 +3,29 @@
 
 var extensionId = bowser.firefox ? '{91f05833-bab1-4fb1-b9e4-187091a4d75d}' : 'ljdobmomdgdljniojadhoplhkpialdid';
 
+extensionId = 'pioaoiklikofpjdencoihhhddobccbac';
+
 function register() {
-  chrome.runtime.sendMessage(
-      extensionId,
-      {
-          type: 'katalon_recorder_register',
-          payload: {
-              summary: 'Sample Katalon Recorder Plugin Format'
-          }
-      }
-  );
+    chrome.runtime.sendMessage(
+        extensionId,
+        {
+            type: 'katalon_recorder_register',
+            payload: {
+                capabilities: [
+                    {
+                        id: 'plain-text', // unique ID for each capability
+                        summary: 'Plain text', // to show in the UI
+                        type: 'export' // for now only 'export' is available
+                    },
+                    {
+                        id: 'json',
+                        summary: 'JSON',
+                        type: 'export'
+                    }
+                ]
+            }
+        }
+    );
 }
 
 register();
@@ -21,16 +34,36 @@ setInterval(register, 60 * 1000);
 
 chrome.runtime.onMessageExternal.addListener(function(message, sender, sendResponse) {
     if (message.type === 'katalon_recorder_export') {
-        var commands = message.payload.commands;
+        var payload = message.payload;
+        var commands = payload.commands;
         var content = '';
-        for (var i = 0; i < commands.length; i++) {
-            var command = commands[i];
-            content += command.command + ' | ' + command.target + ' | ' + command.value + '\n';
+        var extension = '';
+        var mimetype = '';
+        switch (payload.capabilityId) {
+            case 'plain-text':
+                for (var i = 0; i < commands.length; i++) {
+                    var command = commands[i];
+                    content += command.command + ' | ' + command.target + ' | ' + command.value + '\n';
+                }
+                extension = 'txt';
+                mimetype = 'text/plain';
+                break;
+            case 'json':
+                content = JSON.stringify(commands);
+                extension = 'json';
+                mimetype = 'application/ld-json';
+                break;
+            default:
+                content = 'Invalid capability ID';
+                extension = 'txt';
+                mimetype = 'text/plain';
         }
         sendResponse({
             status: true,
             payload: {
-                content: content
+                content: content,
+                extension: extension,
+                mimetype: mimetype
             }
         });
     }
